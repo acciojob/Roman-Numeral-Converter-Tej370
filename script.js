0b3a1619a9792d0f03beaa6f75d524c7a27d477a
Tej370 <153205267+Tej370@users.noreply.github.com>
2024-12-13 12:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-  //your code here
-	 let result = '';
-    
-    // Iterate over the symbols and values
-    for (let i = 0; i < romanNumerals.length; i++) {
-        while (num >= romanNumerals[i][1]) {
-            result += romanNumerals[i][0];
-            num -= romanNumerals[i][1];
-        }
-    }
-    
-    return result;
-
+let standard = [
+	["I",1]
+	["IV",4]
+	["V",5]
+    ["IX",9]
+	["X",10]
+	["XL",40]
+	["L",50]
+	["XC",90]
+	["C",100]
+	["CD",400]
+	["D",500]
+	["CM",900]
+	["M",1000]
+]
+function getCloserValue(n) {
+	for (let i = 0; i < standard.length; i++) {
+		let currentVal = standard[i][1];
+		if (currentVal === n) {
+			return standard[i][0];
+		}
+if (currentVal>n) {
+	return standard[i-1][0];
+	
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+	
+	}
+		ans = "";
+		while (n!=0) {
+			let roman = getCloserValue(n);
+			ans += roman[0];
+			n -= roman[1];
+		}
+	console.log(ans);
+}
